02432affe292500c50a7c3a7b64be36d60a83f9f
Niete <nieteis@gmail.com>
2023-06-05 14:18:37 +0100

    Refactored both files so that it is easier to read the code

--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,47 +1,52 @@
 const assertEquals = require("./assert-equals");
 
-describe("assertEquals", () => {
-  describe("when expected and actual are the same", () => {
-    it("returns without throwing an error", () => {
-      expect(() => assertEquals("abc", "abc")).not.toThrow();
-      expect(() => assertEquals("1", "1")).not.toThrow();
-      expect(() => assertEquals(1, 1)).not.toThrow();
-      expect(() => assertEquals([1, 2, 3], [1, 2, 3])).not.toThrow();
-    });
-  });
+// Test with String
+try {
+  assertEquals("abc", "abc"); // Passes
+} catch (err) {
+  console.error(err.message);
+}
 
-  describe("when expected and actual are different", () => {
-    it("throws an error", () => {
-      expect(() => assertEquals("1", "2")).toThrow(Error);
-      expect(() => assertEquals(1, "2")).toThrow(Error);
-      expect(() => assertEquals(1, 2)).toThrow(Error);
-      expect(() => assertEquals([1, 2], [1, 3])).toThrow(Error);
-      expect(() => assertEquals([1, 2], 2)).toThrow(Error);
-      expect(() => assertEquals([1, 2], "2")).toThrow(Error);
-    });
-  });
-});
+try {
+  assertEquals("abcef", "abc"); // Throws
+} catch (err) {
+  console.error(err.message);
+}
 
-describe("Comparing object types", () => {
-  describe("when expected and actual are different, it should throw an error", () => {
-    it("returns an error if the type is different", () => {
-      expect(() => assertEquals({ name: "Niete" }, 1)).toThrow(Error);
-      expect(() => assertEquals({ name: "Niete" }, "1")).toThrow(Error);
-      expect(() => assertEquals({ name: "Niete" }, [1, 2, 3])).toThrow(Error);
-      expect(() => assertEquals({ name: "Niete" }, null)).toThrow(Error);
-    });
-    it("returns an error if both objects are not the same length", () => {
-      expect(() =>
-        assertEquals({ name: "Niete" }, { name: "Niete", name: "Steph" })
-      ).toThrow(Error);
-    });
-    it("returns an error if both objects do not have the same key values", () => {
-      expect(() => assertEquals({ name: "Niete" }, { name: "Steph" })).toThrow(
-        Error
-      );
-      expect(() => assertEquals({ name: "Niete" }, { names: "Niete" })).toThrow(
-        Error
-      );
-    });
-  });
-});
+// Test with Number
+try {
+  assertEquals(1, 1); // Passes
+} catch (err) {
+  console.error(err.message);
+}
+
+try {
+  assertEquals(1, 2); // Throws
+} catch (err) {
+  console.error(err.message);
+}
+
+try {
+  assertEquals(1, "1"); // Throws
+} catch (err) {
+  console.error(err.message);
+}
+
+// Test with Array
+try {
+  assertEquals(["a", "b", "c"], ["a", "b", "c"]); // Passes
+} catch (err) {
+  console.error(err.message);
+}
+
+try {
+  assertEquals(["a", "b"], ["a", "b", "c"]); // Throws
+} catch (err) {
+  console.error(err.message);
+}
+
+try {
+  assertEquals(["a", "b"], ["a", "d"]); // Throws
+} catch (err) {
+  console.error(err.message);
+}
